d53bb14874d404f1b48a25b869c498e0fde0996b
任华龙 <11477580+ren-hualong@user.noreply.gitee.com>
2022-09-28 18:47:34 +0800

    第一个按钮完成

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # 这是一个VUE的UI组件库
 作者： 任华龙
+## 使用本框架前，请在CSS中开启border-box
+
+```
+* {
+  box-sizing: border-box;
+}
+
+```
---
+++ src/components/Button.vue
@@ -0,0 +1,28 @@
+<template>
+  <button>按钮</button>
+</template>>
+<script>
+export default {
+
+}
+</script>
+<style>
+button {
+  font-size: var(--font-size);
+  height: var(--button-height);
+  padding: 0 1em;
+  border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);
+  background: var(--button-bg);
+  color: var(--button-color);
+  cursor: pointer;
+}
+
+button:hover {
+  border-color: var(--button-color-hover);
+}
+
+button:active {
+  background-color: var(--button-active-bg)
+}
+</style>
